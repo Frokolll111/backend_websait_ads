036cf603b892987a463b894350a15a46f4a1eb11
xlimitan <xlimitan@yandex.ru>
2023-12-26 22:08:44 +0700

    Исправил оформление контроллеров

--- src/main/java/ru/skypro/homework/controller/CommentController.java
+++ src/main/java/ru/skypro/homework/controller/CommentController.java
@@ -29,23 +29,17 @@ public class CommentController {
 
     @Operation(
             summary = "Получение комментариев объявления",
-            responses = {
-                    @ApiResponse(
+            responses = {@ApiResponse(
                             responseCode = "200",
                             description = "OK",
-                            content = {
-                                    @Content(
-                                            mediaType = MediaType.APPLICATION_OCTET_STREAM_VALUE,
-                                            schema = @Schema(implementation = Comments.class)
-                                    )}),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "Unauthorized"
-                    ),
-                    @ApiResponse(
-                            responseCode = "404",
-                            description = "Not found"
-                    )})
+                            content = @Content(mediaType = "application/json",
+                                            schema = @Schema(implementation = Comments.class))),
+                    @ApiResponse(responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content(schema = @Schema(hidden = true))),
+                    @ApiResponse(responseCode = "404",
+                            description = "Not found",
+                            content = @Content(schema = @Schema(hidden = true)))})
     @GetMapping("/{id}/comments")
     public ResponseEntity<Comments> getComment(@PathVariable Integer id) {
         Comments comments = commentService.getCommentsByAdId(id);
@@ -54,23 +48,18 @@ public class CommentController {
 
     @Operation(
             summary = "Добавление комментария к объявлению",
-            responses = {
-                    @ApiResponse(
+            responses = {@ApiResponse(
                             responseCode = "200",
                             description = "OK",
-                            content = {
-                                    @Content(
-                                            mediaType = MediaType.APPLICATION_OCTET_STREAM_VALUE,
+                            content = @Content(mediaType = MediaType.APPLICATION_OCTET_STREAM_VALUE,
                                             schema = @Schema(implementation = CommentDto.class)
-                                    )}),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "Unauthorized"
-                    ),
-                    @ApiResponse(
-                            responseCode = "404",
-                            description = "Not found"
-                    )})
+                            )),
+                    @ApiResponse(responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content(schema = @Schema(hidden = true))),
+                    @ApiResponse(responseCode = "404",
+                            description = "Not found",
+                            content = @Content(schema = @Schema(hidden = true)))})
     @PostMapping("/{id}/comments")
     public ResponseEntity<CommentDto> addComment(@PathVariable Integer id,
                                                   CreateOrUpdateComment createOrUpdateComment,
@@ -81,23 +70,14 @@ public class CommentController {
 
     @Operation(
             summary = "Удаление комментария",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "OK"
-                    ),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "Unauthorized"
-                    ),
-                    @ApiResponse(
-                            responseCode = "403",
-                            description = "Forbidden"
-                    ),
-                    @ApiResponse(
-                            responseCode = "404",
-                            description = "Not found"
-                    )})
+            responses = {@ApiResponse(responseCode = "200",
+                            description = "OK"),
+                    @ApiResponse(responseCode = "401",
+                            description = "Unauthorized"),
+                    @ApiResponse(responseCode = "403",
+                            description = "Forbidden"),
+                    @ApiResponse(responseCode = "404",
+                            description = "Not found")})
     @DeleteMapping("/{adId}/comments/{commentId}")
     public ResponseEntity<Void> deleteComment(@PathVariable Integer adId,
                                               @PathVariable Integer commentId) {
@@ -107,27 +87,19 @@ public class CommentController {
 
     @Operation(
             summary = "Обновление комментария",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
+            responses = {@ApiResponse(responseCode = "200",
                             description = "OK",
-                            content = {
-                                    @Content(
-                                            mediaType = MediaType.APPLICATION_OCTET_STREAM_VALUE,
-                                            schema = @Schema(implementation = CommentDto.class)
-                                    )}),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "Unauthorized"
-                    ),
-                    @ApiResponse(
-                            responseCode = "403",
-                            description = "Forbidden"
-                    ),
-                    @ApiResponse(
-                            responseCode = "404",
-                            description = "Not found"
-                    )})
+                            content = @Content(mediaType = MediaType.APPLICATION_OCTET_STREAM_VALUE,
+                                            schema = @Schema(implementation = CommentDto.class))),
+                    @ApiResponse(responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content(schema = @Schema(hidden = true))),
+                    @ApiResponse(responseCode = "403",
+                            description = "Forbidden",
+                            content = @Content(schema = @Schema(hidden = true))),
+                    @ApiResponse(responseCode = "404",
+                            description = "Not found",
+                            content = @Content(schema = @Schema(hidden = true)))})
     @PatchMapping("/{adId}/comments/{commentId}")
     public ResponseEntity<CommentDto> updateComment(@PathVariable Integer adId,
                                                     @PathVariable Integer commentId,
--- src/main/java/ru/skypro/homework/repository/CommentRepository.java
+++ src/main/java/ru/skypro/homework/repository/CommentRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
-    List<Comment> findAllByAd_Pk(Integer adId);
+    List<Comment> findAllByAdPk(Integer adId);
 
     void deleteAllCommentByPk(int pk);
 
